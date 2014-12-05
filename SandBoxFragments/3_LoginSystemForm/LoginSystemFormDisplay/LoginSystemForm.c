cf8981af143a6d3902fbbfd625f14bae6aafe0d0
SoMysrie <rethores@gmail.com>
2014-12-05 14:02:38 +0100

    Modification de l'identification
    Partage du projet de l'identification en deux. Renommer le dossier pour
    dire que c'est juste l'affichage.

---
+++ SandBoxFragments/3_LoginSystemForm/LoginSystemFormDisplay/LoginSystemForm.c
@@ -0,0 +1,17 @@
+#include "header.h"
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+};
---
+++ SandBoxFragments/3_LoginSystemForm/LoginSystemFormDisplay/header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+#include "LoginSystemForm.h"
+
+#endif // HEADER_H_INCLUDED
