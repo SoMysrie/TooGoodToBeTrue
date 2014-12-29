e44202b4ead1dc25854c5cf0c073763aaefbdd5a
SoMysrie <rethores@gmail.com>
2014-12-29 15:50:05 +0100

    Développement de la gestion d'événements
    Exemple de ce qui est possible de faire avec la souris et le clavier.

--- SandBoxFragments/5_SdlDisplayAndUseMouseContext/SdlDisplayAndUseMouseContext/main.c
+++ SandBoxFragments/5_SdlDisplayAndUseMouseContext/SdlDisplayAndUseMouseContext/main.c
@@ -5,14 +5,23 @@
 
 int main(int argc, char *argv[])
 {
-    SDL_Surface *ecran = NULL;
+    SDL_Surface *ecran = NULL, *zozor = NULL;
+    SDL_Rect positionZozor;
     SDL_Event event;
     int continuer = 1;
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
     SDL_WM_SetCaption("Gestion des evenements en SDL", NULL);
+
+    zozor = SDL_LoadBMP("zozor.bmp");
+    SDL_SetColorKey(zozor, SDL_SRCCOLORKEY, SDL_MapRGB(zozor->format, 0, 0, 255));
+
+    positionZozor.x = ecran->w / 2 - zozor->w / 2;
+    positionZozor.y = ecran->h / 2 - zozor->h / 2;
+
+    SDL_EnableKeyRepeat(10, 10);
 
     while (continuer)
     {
@@ -22,9 +31,38 @@ int main(int argc, char *argv[])
             case SDL_QUIT:
                 continuer = 0;
                 break;
+            case SDL_MOUSEMOTION:
+                positionZozor.x = event.motion.x;
+                positionZozor.y = event.motion.y;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_UP:
+                        positionZozor.y--;
+                        break;
+                    case SDLK_DOWN:
+                        positionZozor.y++;
+                        break;
+                    case SDLK_RIGHT:
+                        positionZozor.x++;
+                        break;
+                    case SDLK_LEFT:
+                        positionZozor.x--;
+                        break;
+                    case SDLK_ESCAPE:
+                        continuer = 0;
+                        break;
+                }
+                break;
         }
+
+        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+        SDL_BlitSurface(zozor, NULL, ecran, &positionZozor);
+        SDL_Flip(ecran);
     }
 
+    SDL_FreeSurface(zozor);
     SDL_Quit();
 
     return EXIT_SUCCESS;
